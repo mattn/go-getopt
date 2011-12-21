166d99bf66c41f9c847f5b95afa656bc71d25702
mattn <mattn.jp@gmail.com>
2011-12-21 22:26:16 +0900

    first import

---
+++ getopt.go
@@ -0,0 +1,73 @@
+package getopt
+
+import "os"
+import "strings"
+
+const EOF = -1
+
+var OptErr = 1
+var OptInd = 1
+var OptOpt uint8
+var OptArg string
+
+var sp = 1
+
+func Getopt(opts string) int {
+	var c uint8
+	var cp int
+	argv := os.Args
+	argc := len(argv)
+
+	if sp == 1 {
+		if OptInd >= argc ||
+			(len(argv[0]) > 0 && argv[OptInd][0] != '-') ||
+			len(argv[0]) == 1 {
+			return -1
+		} else if argv[OptInd] == "--" {
+			OptInd++
+			return -1
+		}
+	}
+	c = argv[OptInd][sp]
+	OptOpt = c
+	cp = strings.Index(opts, string(c))
+	if c == ':' || cp == -1 {
+		if OptErr != 0 {
+			println(": illegal option --", c)
+		}
+		sp++
+		if len(argv[OptInd]) == sp {
+			OptInd++
+			sp = 1
+		}
+		return '?'
+	}
+	cp++
+	if cp < len(opts) && opts[cp] == ':' {
+		if len(argv[OptInd]) > sp+1 {
+			OptArg = argv[OptInd][sp+1 :-1]
+			OptInd++
+		} else {
+			OptInd++
+			if OptInd >= argc {
+				if OptErr != 0 {
+					println(": option requires an argument --", c)
+				}
+				sp = 1
+				return '?'
+			} else {
+				OptArg = argv[OptInd]
+				OptInd++
+			}
+		}
+		sp = 1
+	} else {
+		sp++
+		if len(argv[OptInd]) == sp {
+			sp = 1
+			OptInd++
+		}
+		OptArg = ""
+	}
+	return int(c)
+}
